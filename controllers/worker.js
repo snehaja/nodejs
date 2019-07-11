386c819dd4a1f165337359a8e72fa2f4c3527059
snehaja <jettysneha7@gmail.com>
2019-07-11 16:39:20 +0530

    final worker

--- controllers/worker.js
+++ controllers/worker.js
@@ -1,5 +1,22 @@
-var Worker= require('../models').Worker;
+var worker= require('../models').Worker;
+const acknowledge = require('../models').Acknowledgement;
 module.exports = 
 {
-    
+    acknowledge_work(req,res)
+    {
+      const updated_ack = req.params.ack;
+      const reqid =req.params.ack_request_id;
+      acknowledge.update({
+         ack:updated_ack
+      },{where:{ack_request_id:reqid}
+    })
+    .then(update => res.json({
+       error:false,
+       message:'ack done'
+    }))
+    .catch(error=>res.json({
+        error:true,
+        error:error
+    }))
+    }
 }
