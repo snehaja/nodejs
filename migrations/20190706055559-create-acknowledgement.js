473d8f677f27d3a8d9160a1a45923cc280c6b08e
snehaja <jettysneha7@gmail.com>
2019-07-11 16:43:43 +0530

    Update 20190706055559-create-acknowledgement.js

--- migrations/20190706055559-create-acknowledgement.js
+++ migrations/20190706055559-create-acknowledgement.js
@@ -1,22 +1,22 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Acknowledges', {
-      id:{
+    return queryInterface.createTable('Acknowledgements', {
+      id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-     ack_request_id: 
-     {
-      type: Sequelize.INTEGER
-      },
-      ack_worker_id: {
+      ack_request_id: {
         type: Sequelize.INTEGER
       },
-      ack: {
-        type: Sequelize.INTEGER
+      ack_worker_uname: {
+        type: Sequelize.STRING
+      },
+      ack:{
+        type: Sequelize.INTEGER,
+        defaultValue:"0"
       },
       createdAt: {
         allowNull: false,
@@ -29,6 +29,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Acknowledges');
+    return queryInterface.dropTable('Acknowledgements');
   }
 };
